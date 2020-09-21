241d5b7601314dfbc1fa1c0ccf646ffb7b149ef8
serpient <littlegangwolf@gmail.com>
2020-09-20 22:56:19 -0700

    Add test websocket consoles

--- src/landing-page/index.tsx
+++ src/landing-page/index.tsx
@@ -9,6 +9,7 @@ export const LandingPage = ({
   displayName: string
   setDisplayName: (name: string) => void
 }): JSX.Element => {
+  const history = useHistory()
   const inputName = 'displayNameInput'
 
   const handleInputChange = (e: any): void => {
@@ -16,7 +17,24 @@ export const LandingPage = ({
     setDisplayName(e.currentTarget.value)
   }
 
-  const history = useHistory()
+  const handleSubmit = (): void => {
+    history.push('/chat')
+  }
+
+  const chatServiceEndpoint =
+    process.env.NODE_ENV === 'production'
+      ? 'ws://intense-plateau-11880.herokuapp.com'
+      : 'ws://localhost:8080'
+
+  const websocket = new WebSocket(chatServiceEndpoint)
+
+  websocket.onopen = function () {
+    console.log('WebSocket Client Connected')
+    websocket.send('Hi this is web client.')
+  }
+  websocket.onmessage = function (e) {
+    console.log("Received: '" + e.data + "'")
+  }
 
   return (
     <div className="display-name-form">
@@ -29,7 +47,7 @@ export const LandingPage = ({
           onChange={e => handleInputChange(e)}
         />
       </label>
-      <button onClick={() => history.push('/chat')}>Enter Chat Room</button>
+      <button onClick={() => handleSubmit()}>Enter Chat Room</button>
     </div>
   )
 }
